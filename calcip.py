eb1bf0a149a3eb8fd6f8f21d20ebc1bbe359a2c9
sergio <sergionirvana2010@hotmail.com>
2017-04-04 09:55:25 -0300

    Calculadora de IP

--- calcip.py
+++ calcip.py
@@ -1,44 +1,60 @@
+#!/bin/python3
+# -*- coding: utf8 -*-
+
 ip = input('IP: ').split('.')
 mask = input('Mask: ').split('.')
 
 if(len(ip) == 4 and len(mask) == 4):
-    broadcast, ip_rede = [], []
-    r, b, bin_rede, bin_broadcast, bin_ip = '', '', '', '', ''
+    valida_rede = True
+    valida_mask = True
 
+    for i in ip:
+        if(len(i) > 3):
+            valida_rede = False
+            break
+    
+    for i in mask:
+        if(len(i) > 3):
+            valida_mask = False
+            break
 
-    for i in range(len(ip)):
-        ip_rede.append(int(ip[i]) & int(mask[i]))
-        broadcast.append((~int(mask[i])&0xff) | int(ip_rede[i]))
+    if(valida_rede and valida_mask):
+        broadcast, ip_rede = [], []
+        r, b, bin_rede, bin_broadcast, bin_ip = '', '', '', '', ''
 
-    for i in range(len(ip_rede)):
-        if(i <= 3):
-            r += str(ip_rede[i]) + '.'
-            b += str(broadcast[i]) + '.'
-        else:
-            r += str(ip_rede[i])
-            b += str(broadcast[i])
+        for i in range(len(ip)):
+            ip_rede.append(int(ip[i]) & int(mask[i]))
+            broadcast.append((~int(mask[i])&0xff) | int(ip_rede[i]))
 
-        aux = ip_rede[i]
-        binario = str(bin(int(aux)))
-        bin_rede += binario[2:]
+        for i in range(len(ip_rede)):
+            if(i <= 3):
+                r += str(ip_rede[i]) + '.'
+                b += str(broadcast[i]) + '.'
+            else:
+                r += str(ip_rede[i])
+                b += str(broadcast[i])
 
-        aux = ip[i]
-        binario = str(bin(int(aux)))
-        bin_ip += binario[2:]
+            aux = ip_rede[i]
+            binario = str(bin(int(aux)))
+            bin_rede += binario[2:]
 
-        aux = broadcast[i]
-        binario = str(bin(int(aux)))
-        bin_broadcast += binario[2:]
+            aux = ip[i]
+            binario = str(bin(int(aux)))
+            bin_ip += binario[2:]
 
+            aux = broadcast[i]
+            binario = str(bin(int(aux)))
+            bin_broadcast += binario[2:]
 
+        ip = ".".join(ip)
+        mask = '.'.join(mask)
 
-    ip = ".".join(ip)
-    mask = '.'.join(mask)
-
-    print('Rede {0}, Ip {1}, BroadCast {2}, Mascara {3}'.format(r, ip, b, mask))
-    print('Binário da rede: {0}'.format(bin_rede))
-    print('Binário do host: {0}'.format(bin_ip))
-    print('Binário do brodcast: {0}'.format(bin_broadcast))
+        print('Rede {0}, Ip {1}, BroadCast {2}, Mascara {3}'.format(r, ip, b, mask))
+        print('Binário da rede: {0}'.format(bin_rede))
+        print('Binário do host: {0}'.format(bin_ip))
+        print('Binário do brodcast: {0}'.format(bin_broadcast))
+    else:
+        print('Ip e/ou mascara inválidos!')
 
 else:
-    print('Ip e/ou mascara invalidos!')
+    print('Ip e/ou mascara inválidos!')
